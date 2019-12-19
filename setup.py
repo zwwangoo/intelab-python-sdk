df6151f20e4ec7c9d93e8ac5a4f5b36bf5742bdd
王志文 <w_angzhiwen@163.com>
2019-12-19 16:17:39 +0800

    description-content-type

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+from codecs import open
 from setuptools import setup, find_packages
 
 
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name='intelab_python_sdk',
-    version='0.3.1',
+    version='0.3.3',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='iLabService',
+    author_email='ils@iLabService.com',
     packages=find_packages(exclude=('tests', 'tests.*')),
     zip_safe=False,
     include_package_data=True,
